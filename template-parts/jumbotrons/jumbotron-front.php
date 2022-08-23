f8d7d3691cf7879fa301f8629bde8fb8fdcd50e4
yvonbenahita <yvonbenahita@gmail.com>
2022-08-23 21:59:47 +0300

    Add abstract bg

---
+++ template-parts/jumbotrons/jumbotron-front.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * jumbotron.php
+ * Template for dispaying the jumbotron
+ */
+?>
+<!-- Jumbotron -->
+<div class="container-fluid text-center">
+    <div class="jumbotron">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <h1>
+                    <!--						--><?php //esc_html_e( 'Hello, my name is Cory Simmons.', DOMAIN ); ?>
+					<?php esc_html_e( get_theme_mod( 'jumbotron-title', esc_html__( 'Hello, my name is Yvon Aulien Benahita.', DOMAIN ) ) ); ?>
+                </h1>
+                <p class="lead">
+                    <!--						--><?php //esc_html_e( 'I sell websites and website accessories.', DOMAIN ); ?>
+					<?php esc_html_e( get_theme_mod( 'jumbotron-subtitle', esc_html__( 'I\'m a WordPress Expert Developer.', DOMAIN ) ) ); ?>
+                </p>
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+    </div> <!-- End jumbotron -->
+</div> <!-- End container-fluid -->
