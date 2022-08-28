b650728a96e7b84f92ec29a81db8ad4a7513de19
yvonbenahita <yvonbenahita@gmail.com>
2022-08-28 14:34:03 +0300

    Contac Page ok

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -17,7 +17,32 @@ get_header();
 <!-- Contact Form -->
 <div id="contact-form-wprap" class="contact-form-wrap container-fluid">
 	<div class="row">
-		<?php echo do_shortcode( '[wpforms id="222" title="false"]' ); ?>
+		<!-- Check if WP Forms is installed and active -->
+		<?php if ( in_array('wpforms-lite/wpforms.php', apply_filters('active_plugins', get_option('active_plugins'))) ) : ?>
+			<!-- On récupère ID du form -->
+			<?php 
+				$action_scheduler_hybrid_store_demarkation = get_option( 'action_scheduler_hybrid_store_demarkation' );
+				$form_infos = get_option( '_transient_wpforms_dash_widget_lite_entries_by_form' );
+				$form_title = ( string ) $form_infos[$action_scheduler_hybrid_store_demarkation]['title'];
+
+				if ( strcasecmp( $form_title, 'contact ' ) ) {
+					$form_id =  $form_infos[$action_scheduler_hybrid_store_demarkation]['form_id'];
+				}else {
+					esc_html_e( 'Oups, pas de formulaire.', DOMAIN );
+					echo '<br>';
+					esc_html_e( 'IMPORTANT : Le titre du formulaire doit contenir le mot clé "contact".', DOMAIN );
+				}
+			?>
+
+			<?php echo do_shortcode( '[wpforms id="' . $form_id . '"]' ); ?>
+
+		<?php else: ?>	
+			<?php 
+				esc_html_e( 'Veuillez installer puis activer le plugin WP Forms, c\'est obligatoire pour cet thème. Merci.', DOMAIN );
+				echo '<br>';
+				esc_html_e( 'IMPORTANT : Le titre du formulaire doit contenir le mot clé "contact".', DOMAIN );
+			?>
+		<?php endif; ?>
 	</div>
 </div>
 
