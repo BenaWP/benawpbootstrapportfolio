4e8716233ac398f3f34a2dc060650c843afb85a4
yvonbenahita <yvonbenahita@gmail.com>
2022-08-31 16:04:46 +0300

    Add required plugin,  arrary

--- functions.php
+++ functions.php
@@ -299,27 +299,17 @@ add_action( 'tgmpa_register', 'benawp_register_required_plugins' );
 function benawp_register_required_plugins() {
 
 	$plugins = array(
-
-		// This is an example of how to include a plugin bundled with a theme.
 		array(
-			'name'               => 'Visual Composer',
-			// The plugin name.
-			'slug'               => 'visual_composer',
-			// The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory_uri() . '/lib/plugins/visualcomposer.44.3.1.zip',
-			// The plugin source. It can be an external link, WordPress plugin repository or a GITHUB repository.
-			'required'           => false,
-			// If false, the plugin is only 'recommended' instead of required.
-			'version'            => '',
-			// E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false,
-			// If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false,
-			// If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '',
-			// If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '',
-			// If set, this callable will be checked for availability to determine if a plugin is active.
+			'name'               => 'WPForms Lite',  // The plugin name.
+			'slug'               => 'wpforms-lite',  // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory_uri() . '/inc/plugins/wpforms-lite.1.7.6.zip',  // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '',    // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '',    // If set, this callable will be checked for availability to determine if a plugin is active.
+
 		),
 	);
 
