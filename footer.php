d47dc93dc575e91044f287639c4c6ecbe872426b
yvonbenahita <yvonbenahita@gmail.com>
2022-09-05 11:26:48 +0300

    Adding copyright footer

--- footer.php
+++ footer.php
@@ -8,9 +8,12 @@
 
 <footer class="container-fluid site-footer">
     <div class="row">
-        <div class="col-md-12 contact-infos">
+        <div class="col-md-6 contact-infos">
 			<?php if ( is_active_sidebar( 'footer-wiget-tel' ) ) : ?><?php dynamic_sidebar( 'footer-wiget-tel' ); ?><?php endif; ?>
 			<?php if ( is_active_sidebar( 'footer-wiget-mail' ) ) : ?><?php dynamic_sidebar( 'footer-wiget-mail' ); ?><?php endif; ?>
+        </div>
+        <div class="col-md-6 copyright">
+            <span> Copyright © <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> </span>
         </div>
     </div> <!-- end row -->
 </footer>
