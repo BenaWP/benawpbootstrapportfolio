340643b0f102a33a6380f3958d7fbe21ce71f4a8
yvonbenahita <yvonbenahita@gmail.com>
2022-09-27 08:42:54 +0300

    Theme Sniffer TS, jumbotron-front

--- template-parts/jumbotron/jumbotron-front.php
+++ template-parts/jumbotron/jumbotron-front.php
@@ -10,10 +10,10 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <h1>
-					<?php esc_html_e( get_theme_mod( 'jumbotron-front-title', esc_html__( 'Hello, mon nom est remplacer par le votre.', 'benawp-bootstrap-portfolio' ) ) ); ?>
+					<?php echo esc_html( get_theme_mod( 'jumbotron-front-title', esc_html__( 'Hello, mon nom est remplacer par le votre.', 'benawp-bootstrap-portfolio' ) ) ); ?>
                 </h1>
                 <p class="lead">
-					<?php esc_html_e( get_theme_mod( 'jumbotron-front-subtitle', esc_html__( 'Ensuite dis nous ce que vous faites.', 'benawp-bootstrap-portfolio' ) ) ); ?>
+					<?php echo esc_html( get_theme_mod( 'jumbotron-front-subtitle', esc_html__( 'Ensuite dis nous ce que vous faites.', 'benawp-bootstrap-portfolio' ) ) ); ?>
                 </p>
             </div> <!-- End col -->
         </div> <!-- End row -->
